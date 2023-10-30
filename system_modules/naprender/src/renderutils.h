75f288f851da1500ad5928e0104db21cd9517c07
lshoek <lesleyvanhoek@hotmail.com>
2023-10-30 15:44:09 +0100

    add optional depth texture property to render target

--- system_modules/naprender/src/renderutils.h
+++ system_modules/naprender/src/renderutils.h
@@ -124,6 +124,11 @@ namespace nap
 	bool NAPAPI createRenderPass(VkDevice device, VkFormat colorFormat, VkFormat depthFormat, VkSampleCountFlagBits samples, VkImageLayout targetLayout, VkRenderPass& renderPass, utility::ErrorState& errorState);
 
 	/**
+	 * Creates a single or multi-sample renderpass based on rasterization samples and color/depth formats.
+	 */
+	bool NAPAPI createRenderPass(VkDevice device, VkFormat colorFormat, VkFormat depthFormat, VkSampleCountFlagBits samples, VkImageLayout targetLayout, bool consumeDepth, VkRenderPass& renderPass, utility::ErrorState& errorState);
+
+	/**
 	 * Creates a single or multi-sample depth-only renderpass based depth format.
 	 */
 	bool NAPAPI createDepthOnlyRenderPass(VkDevice device, VkFormat depthFormat, VkRenderPass& renderPass, utility::ErrorState& errorState);
